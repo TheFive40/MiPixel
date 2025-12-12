74e49094c3dad541bdb1167f9665bc8ab9ab097b
jeanf <jeanf2805@gmail.com>
2025-12-11 22:39:35 -0500

    Initial commit

---
+++ src/main/java/com/example/mipixel/MiPixelApplication.java
@@ -0,0 +1,13 @@
+package com.example.mipixel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPixelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiPixelApplication.class, args);
+    }
+
+}
